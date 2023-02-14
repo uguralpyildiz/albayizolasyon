80d3158bb76bdc609151ad232b88d2f5e727dd38
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-02-14 21:07:18 +0300

    contentfolders add

---
+++ script.js
@@ -0,0 +1,16 @@
+const menubtn = document.querySelector(".menubtn")
+const overlay = document.querySelector(".overlay")
+const navbar = document.querySelector(".navbar")
+const body = document.querySelector("body")
+
+menubtn.addEventListener("click", ()=>{
+    body.style.overflow = "hidden";
+    overlay.style.display = "block";
+    navbar.style.left = "0";
+})
+
+overlay.addEventListener("click", () => {
+    body.style.overflow = "auto";
+    navbar.style.left = "-500px";
+    overlay.style.display = "none";
+})
