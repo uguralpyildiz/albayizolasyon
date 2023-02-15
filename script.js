2c781238c59c825e5131a32625737e953ac07c30
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-02-15 15:51:11 +0300

    feelblue

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menubtn = document.querySelector(".menubtn")
 const overlay = document.querySelector(".overlay")
 const navbar = document.querySelector(".navbar")
 const body = document.querySelector("body")
+const link = document.querySelectorAll(".links a")
 
 menubtn.addEventListener("click", ()=>{
     body.style.overflow = "hidden";
@@ -9,8 +10,16 @@ menubtn.addEventListener("click", ()=>{
     navbar.style.left = "0";
 })
 
+
 overlay.addEventListener("click", () => {
     body.style.overflow = "auto";
     navbar.style.left = "-500px";
     overlay.style.display = "none";
 })
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener("click", () => {
+        body.style.overflow = "auto";
+        navbar.style.left = "-500px";
+        overlay.style.display = "none";
+    })  
+}
